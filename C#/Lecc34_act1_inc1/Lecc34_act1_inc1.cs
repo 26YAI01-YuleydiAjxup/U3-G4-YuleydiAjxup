69adac56bc5e65cbe6faa3bd770f30bcd9a9dc0f
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-07-22 16:58:29 -0600

    Add files via upload

---
+++ C#/Lecc34_act1_inc1/Lecc34_act1_inc1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecc34_act1_inc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
